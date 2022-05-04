029ae9e4549c110ef3acc3716dfe083edbc09c19
KJH <stonehead334@gmail.com>
2022-05-04 09:35:51 +0900

    fix

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -9,27 +9,54 @@ import {
   ScrollView,
 } from "react-native";
 import { theme } from "./colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({}); // <- hashmap??
 
+  useEffect(() => {
+    loadToDos();
+  }, []);
+  /*
+  Async Storage는 문자열 데이터만 저장할 수 있으므로 
+  객체 데이터를 저장하려면 먼저 직렬화해야 합니다.
+  JSON으로 직렬화할 수 있는 데이터의 경우 데이터를 저장할 때
+  JSON.stringify()를 사용하고 데이터를 로드할 때
+  JSON.parse()를 사용할 수 있습니다.
+*/
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = payload => setText(payload);
-  const addTodo = () => {
+
+  const saveToDos = async toSave => {
+    //현재 있는 toDos를 string으로 바꿔준다
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  };
+
+  const addTodo = async () => {
     if (text === "") {
       return;
     }
     // work에서 온것과 travel에서 온 것을 구별하기 위해
     // 첫번째 값은 수정할 수 없으니 set을 이용해서
     // 전에 쓰던걸 새로운 것에 붙여야 된다
-    const newTodos = { ...toDos, [Date.now()]: { text, work: working }, };
+    const newTodos = { ...toDos, [Date.now()]: { text, working } };
     setToDos(newTodos);
     //save Todo
+    await saveToDos(newTodos);
     setText("");
   };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -53,24 +80,30 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
-        onSubmitEditing={addTodo}
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
         returnKeyType="done"
         value={text}
-        placeholder={working ? "Add To do!!" : "Where do you want to go?"}
+        placeholder={
+          working ? "What do you have to do?" : "Where do you want to go?"
+        }
         style={styles.input}
       />
       <ScrollView>
-        {/*keys로 object{}를 가져온다. map으로 각 항목을 경유하고, 내가 보낸 텍스트를 보여주고 있다 */}
-        {Object.keys(toDos).map(key => (
-          <View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
-          </View>
-        ))}
+        {Object.keys(toDos).map(key =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
 }
+
+// 각 화면에 넘어가면 적은 텍스트가 구분된다
+/*keys로 object{}를 가져온다. map으로 각 항목을 경유하고, 내가 보낸 텍스트를 보여주고 있다 */
 
 const styles = StyleSheet.create({
   container: {
